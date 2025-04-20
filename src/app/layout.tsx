5deb80509ba224ec4a818be30af7bc6a3736f0d6
teoMarinov <teodor.b.marinov@gmail.com>
2025-04-20 13:01:59 +0300

    add dm-sans font to project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { DM_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const dmSans = DM_Sans({
+  variable: "--font-dm-sans",
+  subsets: ['latin-ext']
+})
 
 export const metadata: Metadata = {
   title: "AdStart Media Task",
@@ -25,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${dmSans.variable} antialiased font-sans`}
       >
         {children}
       </body>
