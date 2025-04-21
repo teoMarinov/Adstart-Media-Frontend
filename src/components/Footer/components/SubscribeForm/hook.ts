41f5772f3c77a7766b914c1c1c269ac20f56aa73
teoMarinov <teodor.b.marinov@gmail.com>
2025-04-21 21:08:17 +0300

    Implement sonner/toaster

--- src/components/Footer/components/SubscribeForm/hook.ts
+++ src/components/Footer/components/SubscribeForm/hook.ts
@@ -1,5 +1,6 @@
 import baseApi from "@/lib/axios";
 import { useForm } from "react-hook-form";
+import { openToast } from "@/utils/toast";
 import { zodResolver } from "@hookform/resolvers/zod";
 import {
   subscribeSchema,
@@ -15,7 +16,14 @@ export default function UseSubscribeForm() {
   });
 
   const onSubmit = (data: SubscribeSchemaValues) => {
-    baseApi.post("/subscribe", data);
+    baseApi
+      .post("/subscribe", data)
+      .then(() => openToast("Thank you for subscribing!", "Great!"))
+      .catch((error) => {
+        if (error.response.status === 409) {
+          openToast("Already subscribed with this email!", "Close");
+        }
+      });
   };
 
   return {
