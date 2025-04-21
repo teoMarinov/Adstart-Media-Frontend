147222669c53743b21760fa26ee4e2e0419bdacd
teoMarinov <teodor.b.marinov@gmail.com>
2025-04-21 16:07:06 +0300

    add max w

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import ProjectQuoteForm from "@/components/ProjectQuoteForm";
 
 export default function Home() {
   return (
-    <div className={`min-h-[calc(100vh-${headerHeight}px)] md:min-h-0 h-full grid place-items-center`}>
-      <div className="flex flex-col items-center p-4">
+    <div className={`min-h-[calc(100vh-${headerHeight}px)] md:min-h-0 h-full grid place-items-center `}>
+      <div className="flex flex-col items-center p-4 max-w-screen">
         <div className="mb-5">
           <h1 className="text-2xl font-bold text-center">
             Get a project quote
